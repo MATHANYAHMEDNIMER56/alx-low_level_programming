aaee8777cecf68fa9a2967c1b01f9cb9bcadaf2f
MathanyNimer <m.nimer@exp-sa.com>
2023-09-08 03:24:17 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
- * main - this is the main function
- * 
+ * main - this is the main function 
  * Description: 'Programming is like building'
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
