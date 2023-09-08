aaabb6e4c88e392b3b1f61dfa441df04f550bea9
MathanyNimer <m.nimer@exp-sa.com>
2023-09-08 05:28:25 +0300

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+#include <unisted.h>
+/**
+ * main - this is the main function
+ * Description: 'that piece of art is useful'
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	return (1);
+}
