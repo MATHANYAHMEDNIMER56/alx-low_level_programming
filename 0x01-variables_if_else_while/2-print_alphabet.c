9d0041f209221fe228abc6a4f323ff7d55ec0e40
MathanyNimer <m.nimer@exp-sa.com>
2023-09-11 03:21:43 +0300

    lower alphaa

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /* betty style doc for function main goes there */
 /**
   *main - this function to get lower alpha
-  *Return:  Always 0
+  *Return: Always 0
   */
 int main(void)
 {
-	char i ;
-
+	char i;
 	for (i = 'a' ; i <= 'z' ;  i++)
 		putchar(i);
 	putchar('\n');
